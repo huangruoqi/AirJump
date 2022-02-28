b37f4a8b4ae44cccc897a82543193cdbdf0c6a1d
Ruoqi Huang <huangruoqi374@gmail.com>
2022-02-27 19:05:03 -0800

    movement done

--- Game/index.js
+++ Game/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import * as React from "react";
 import { View, Text, StatusBar, Dimensions } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import { Update, Touch, Collide } from "./systems";
 import { Box } from "./renderers";
+import { Accelerometer } from 'expo-sensors';
 
 const { width, height } = Dimensions.get("screen");
 
@@ -10,7 +11,7 @@ class Player {
 	constructor(x, y) {
 		this.p = {x: x, y: y}
 		this.v = {x: 0, y: 0}
-		this.gravity = -800
+		this.gravity = -1000
 		this.a = {x: 0, y: this.gravity}
 		this.size = {width: Math.trunc(height / 8), height: Math.trunc(height / 8)}
 		this.canJump = true
@@ -20,22 +21,39 @@ class Player {
 class Platform {
 	constructor(x, y) {
 		this.p = {x: x, y: y}
-		this.v = {x: 0, y: 0}
+		this.v = {x: -200, y: 0}
 		this.a = {x: 0, y: 0}
-		this.size = {width: Math.trunc(height / 2), height: Math.trunc(height / 8)}
+		this.size = {width: Math.trunc(height / 2.5), height: Math.trunc(height / 10)}
 	}
 }
 
 
 const RigidBodies = (props) => {
+	const ref = React.useRef({x:0, y:0, z:0})
+  React.useEffect(() => {
+		const id = Accelerometer.addListener(accelerometerData => {
+			ref.current = accelerometerData
+		})
+		Accelerometer.setUpdateInterval(20)
+    return () => id.remove();
+  }, []);
+
 	const player = new Player(0, 100) 
 	const floor = new Platform(0, -100)
+
+	const Acc = function(state) {
+		const player = state['player'].body
+		player.v.x = ref.current.y * -800
+		return state
+	}
+
   return (
     <GameEngine
-      systems={[Update, Touch, Collide]}
+      systems={[Update, Touch, Collide, Acc]}
       entities={{
-				player: {body: player, color: 'dodgerblue', renderer: Box},
-				floor: {body: floor, renderer: Box}
+				engine: {},
+				player: {body: player, color: 'dodgerblue', renderer: Box, totalTime: 0},
+				floor: {body: floor, floorID: -1, renderer: Box}
       }}
     >
       <StatusBar hidden={true} />
--- Game/systems.js
+++ Game/systems.js
@@ -1,23 +1,61 @@
-const Update = (state, { touches, time, screen }) => {
+import {Dimensions} from 'react-native'
+import { Box } from "./renderers";
+
+let floorID = 1
+const {width, height} = Dimensions.get('screen')
+
+const Update = (state, { touches, time, screen}) => {
 	const body = state['player'].body
 	const dt = time.delta / 1000;
 	body.v.x += body.a.x * dt
 	body.v.y += body.a.y * dt
-	body.v.y = Math.max(-500, body.v.y)
+	body.v.y = Math.max(-600, body.v.y)
 	body.p.x += body.v.x * dt
+	body.p.x = Math.max(-width/2 +50, body.p.x)
+	body.p.x = Math.min(width/2 -50, body.p.x)
 	body.p.y += body.v.y * dt
 	body.canJump = body.v.y < 0
-	
-		
+	state['player'].totalTime += time.delta	
+	if (state['player'].totalTime >= 1300) {
+		state['player'].totalTime = 0	
+		state[floorID] = {
+			body: new Platform(200+width / 2,Math.trunc(Math.random() * 50) - 100),
+			floorID: floorID,
+			renderer: Box
+		}
+		floorID++;
+	}
+
+	Object.keys(state).filter(key => state[key].floorID).forEach(key => {
+		const b = state[key].body
+		const dt = time.delta / 1000;
+		b.v.x += b.a.x * dt
+		b.v.y += b.a.y * dt
+		b.p.x += b.v.x * dt
+		b.p.y += b.v.y * dt
+		if (b.p.x < -width / 2 -200) {
+			delete state[key]
+		}
+	})
 	return state;
 };
+
+
+class Platform {
+	constructor(x, y) {
+		this.p = {x: x, y: y}
+		this.v = {x: -200, y: 0}
+		this.a = {x: 0, y: 0}
+		this.size = {width: Math.trunc(height / 2.5), height: Math.trunc(height / 10)}
+	}
+}
 
 const Touch = (state, { touches, time, screen}) => {
 	const press = touches.find(t => t.type == 'press')
 	const player = state['player'].body
 	if (press) {
 		if (player.a.y != 0)
-			player.v.y = 400
+			player.v.y = 450
 	}
 	return state;
 }
@@ -28,18 +66,18 @@ const Collide = (state, { time, screen }) => {
 		const xdis = Math.abs(p.p.x - q.p.x) - (p.size.width + q.size.width) / 2
 		if (xdis > 0) return false
 		const ydis = (p.p.y - q.p.y) - (p.size.height + q.size.height) / 2
-		return ydis > -10 && ydis < 5
+		return ydis > -15 && ydis < 0
 	}
 	const player = state['player'].body
 	Object.keys(state)
-	.filter(key => key != 'player')
+	.filter(key => state[key].floorID)
 	.forEach(key => {
 		if (checkCollision(player, state[key].body)) {
 			player.v.y = 0
 			player.a.y = 0
-			player.p.y = state[key].body.p.y+(player.size.height+state[key].body.size.height)/2
+			player.p.y = (state[key].body.p.y + player.p.y + player.size.height) / 2 
 			setTimeout(() => {
-				player.v.y = 500
+				player.v.y = 550
 				player.a.y = player.gravity
 			}, 150);
 		}
